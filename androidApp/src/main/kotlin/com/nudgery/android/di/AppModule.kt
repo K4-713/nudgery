f63ee05ea1c5f19bda9b771af85a4ba3c1c10fe5
K4-713 <katie@katiehorn.com>
2026-05-31 19:53:56 -0700

    Handle Koin deprecation warning

--- androidApp/src/main/kotlin/com/nudgery/android/di/AppModule.kt
+++ androidApp/src/main/kotlin/com/nudgery/android/di/AppModule.kt
@@ -12,19 +12,8 @@ import com.nudgery.android.viewmodel.SettingsViewModel
 import com.nudgery.shared.db.DatabaseDriverFactory
 import com.nudgery.shared.scheduler.NotificationScheduler
 import com.nudgery.shared.scheduler.WorkManagerNotificationScheduler
-import com.nudgery.shared.repository.AnswerRepository
-import com.nudgery.shared.repository.NotificationFireRepository
-import com.nudgery.shared.usecase.ComputeNextFireTimeUseCase
-import com.nudgery.shared.usecase.DeleteNudgeUseCase
-import com.nudgery.shared.usecase.ExportAnswersUseCase
-import com.nudgery.shared.usecase.GetVisualizationDataUseCase
-import com.nudgery.shared.usecase.ImportNudgeUseCase
-import com.nudgery.shared.usecase.RecordAnswerUseCase
-import com.nudgery.shared.usecase.SetAnswerHiddenUseCase
-import com.nudgery.shared.usecase.UpdateNudgeUseCase
-import kotlinx.datetime.Instant
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
@@ -33,68 +22,10 @@ val appModule = module {
     single<AppSettings> { DataStoreAppSettings(androidContext()) }
     single { NudgeBackupParser() }
 
-    viewModel {
-        NudgeListViewModel(
-            nudgeRepository = get(),
-            scheduleRepository = get(),
-            answerRepository = get<AnswerRepository>(),
-            notificationFireRepository = get<NotificationFireRepository>(),
-            computeNextFireTime = get<ComputeNextFireTimeUseCase>(),
-            updateNudge = get<UpdateNudgeUseCase>()
-        )
-    }
-
-    viewModel {
-        CreateNudgeViewModel(createNudge = get(), nudgeRepository = get())
-    }
-
-    viewModel { (nudgeId: String) ->
-        NudgeDetailViewModel(
-            nudgeId = nudgeId,
-            nudgeRepository = get(),
-            questionRepository = get(),
-            questionOptionRepository = get(),
-            scheduleRepository = get(),
-            answerRepository = get(),
-            notificationFireRepository = get<NotificationFireRepository>(),
-            computeNextFireTime = get<ComputeNextFireTimeUseCase>(),
-            getVisualizationData = get<GetVisualizationDataUseCase>(),
-            setAnswerHidden = get<SetAnswerHiddenUseCase>(),
-            exportAnswers = get<ExportAnswersUseCase>(),
-            updateNudge = get<UpdateNudgeUseCase>(),
-            deleteNudge = get<DeleteNudgeUseCase>(),
-            appSettings = get<AppSettings>()
-        )
-    }
-
-    viewModel { (nudgeId: String) ->
-        EditNudgeViewModel(
-            nudgeId = nudgeId,
-            nudgeRepository = get(),
-            questionRepository = get(),
-            questionOptionRepository = get(),
-            scheduleRepository = get(),
-            updateNudge = get<UpdateNudgeUseCase>()
-        )
-    }
-
-    viewModel { (nudgeId: String, scheduledAt: Instant?) ->
-        AnswerFormViewModel(
-            nudgeId = nudgeId,
-            scheduledAt = scheduledAt,
-            questionRepository = get(),
-            questionOptionRepository = get(),
-            recordAnswer = get<RecordAnswerUseCase>()
-        )
-    }
-
-    viewModel {
-        SettingsViewModel(
-            appSettings = get(),
-            importNudge = get<ImportNudgeUseCase>(),
-            deleteNudge = get<DeleteNudgeUseCase>(),
-            nudgeRepository = get(),
-            backupParser = get<NudgeBackupParser>()
-        )
-    }
+    viewModelOf(::NudgeListViewModel)
+    viewModelOf(::CreateNudgeViewModel)
+    viewModelOf(::NudgeDetailViewModel)
+    viewModelOf(::EditNudgeViewModel)
+    viewModelOf(::AnswerFormViewModel)
+    viewModelOf(::SettingsViewModel)
 }
